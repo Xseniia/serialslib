5c0307eb6c1fb7981e2b5271c32bf2a9de0e0676
Kseniya Khitro <khitroxs@gmail.com>
2019-07-11 13:38:54 +0300

    feat: added devise gem, added admin to users

--- app/models/serial.rb
+++ app/models/serial.rb
@@ -1,2 +1,9 @@
 class Serial < ApplicationRecord
+  require 'date'
+  DATE = Date.current.year
+
+  validates :title, presence: { message: 'must be filled.' }
+  validates :since, length: { is: 4, message: 'can be only 4 digits long.' }, numericality: { only_integer: true, greater_than_or_equal_to: 1933, less_than_or_equal_to: DATE }
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ApplicationRecord
-  validates :first_name, presence: { message: ' must be filled.' }
-  validates :last_name, presence: { message: ' must be filled.' }
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  require 'date'
+
   validates :gender, acceptance: { accept: ['Male', 'Female', 'male', 'female'], case_sensitive: false, message: "can only be 'Male' or 'Female'."  }
+  validate :date_validation
+
+  def date_validation
+    if date_of_birth > Date.today
+      errors.add(:date_of_birth, "Date of birth musn't be greater than the current date.")
+    end
+  end
 end
