a4a0e90ecaca1aaf1e527c17798b9cfcbf9fe65c
Kseniya Khitro <khitroxs@gmail.com>
2019-07-26 17:28:10 +0300

    fix: set up email verification

---
+++ config/smtp_settings.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+ActionMailer::Base.smtp_settings = {
+  address: 'smtp.gmail.com',
+  port: '587',
+  domain: 'mydomain.net',
+  user_name: 'holygerardscake@gmail.com',
+  password: '2612023555',
+  authentication: :plain,
+  enable_starttls_auto: true
+}
