2e0c6cf197a856b1dd32f0a53f264c5d0ad0cc0a
Kseniya Khitro <khitroxs@gmail.com>
2019-08-06 13:53:04 +0300

    feat: added tag search

--- app/models/serial.rb
+++ app/models/serial.rb
@@ -60,4 +60,8 @@ class Serial < ApplicationRecord # :nodoc:
   def not_favourite?(user_id)
     Favourite.where(user_id: user_id, serial_id: id).empty?
   end
+
+  def self.tagged_with(name)
+    Tag.find_by_tag_name(name).serials
+  end
 end
