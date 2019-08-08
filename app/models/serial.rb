196bc8830cc84f3a1fa8476385d505baabefafbc
Kseniya Khitro <khitroxs@gmail.com>
2019-08-08 17:35:30 +0300

    fix

--- app/models/serial.rb
+++ app/models/serial.rb
@@ -16,8 +16,6 @@ class Serial < ApplicationRecord # :nodoc:
   has_many :view_statuses
   has_many :users_status, through: :view_statuses, source: :user
 
-  delegate :users_status, to: :view_statuses
-
   has_many :serial_genres
   has_many :genres, through: :serial_genres
 
