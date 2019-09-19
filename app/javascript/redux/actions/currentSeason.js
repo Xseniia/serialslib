cab473de6f46a9f0681541a000d47e6d3a1fc748
Kseniya Khitro <khitroxs@gmail.com>
2019-09-19 14:59:08 +0300

    url fix

--- app/javascript/redux/actions/currentSeason.js
+++ app/javascript/redux/actions/currentSeason.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = 'http://localhost:3000/'
+const url = 'https://serialslib.herokuapp.com'
 
 export const collectSeasonData = (id) => {
   return(dispatch => {
